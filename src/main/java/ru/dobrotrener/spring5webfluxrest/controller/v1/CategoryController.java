dab734c82ba658400953806905327e7d985bdf37
Rafael Valiev <feelermorses@gmail.com>
2018-08-27 19:59:07 +0300

    Post/put categories

--- src/main/java/ru/dobrotrener/spring5webfluxrest/controller/v1/CategoryController.java
+++ src/main/java/ru/dobrotrener/spring5webfluxrest/controller/v1/CategoryController.java
@@ -2,9 +2,15 @@ package ru.dobrotrener.spring5webfluxrest.controller.v1;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -32,6 +38,19 @@ public class CategoryController {
     @GetMapping("{id}")
     Mono<Category> getById(@PathVariable String id) {
         return categoryRepository.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> create(@RequestBody  Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream)
+                .then();
+    }
+
+    @PutMapping("{id}")
+    Mono<Category> update(@PathVariable String id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
     }
 
 }
--- src/test/java/ru/dobrotrener/spring5webfluxrest/controller/v1/CategoryControllerTest.java
+++ src/test/java/ru/dobrotrener/spring5webfluxrest/controller/v1/CategoryControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,12 +15,15 @@ import ru.dobrotrener.spring5webfluxrest.domain.Category;
 import ru.dobrotrener.spring5webfluxrest.repository.CategoryRepository;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.contains;
 import static reactor.core.publisher.Mono.when;
 
 public class CategoryControllerTest {
 
+    public static final String SOME_CAT = "SomeCat";
+    public static final String SOME_ID = "id";
     private WebTestClient webTestClient;
     private CategoryRepository categoryRepository;
     private CategoryController categoryController;
@@ -33,12 +37,12 @@ public class CategoryControllerTest {
     }
 
     @Test
-    public void getAll() {
+    public void testGetAll() {
         BDDMockito.given(categoryRepository.findAll())
                 .willReturn(
                         Flux.just(
-                                Category.builder().description("Cat1").build(),
-                                Category.builder().description("Cat2").build()
+                                Category.builder().description(SOME_CAT).build(),
+                                Category.builder().description(SOME_CAT).build()
                         ));
         webTestClient.get()
                 .uri(CategoryController.BASE_URL)
@@ -48,14 +52,43 @@ public class CategoryControllerTest {
     }
 
     @Test
-    public void getById() {
-        BDDMockito.given(categoryRepository.findById("SomeCatId"))
+    public void testGetById() {
+        BDDMockito.given(categoryRepository.findById(anyString()))
                 .willReturn(
-                        Mono.just(Category.builder().description("SomeCatId").build())
+                        Mono.just(Category.builder().description(SOME_CAT).build())
                 );
         webTestClient.get()
-                .uri(CategoryController.BASE_URL + "id")
+                .uri(CategoryController.BASE_URL + SOME_ID)
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void testCreate() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description(SOME_CAT).build());
+
+        webTestClient.post()
+                .uri(CategoryController.BASE_URL)
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdate() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description(SOME_CAT).build());
+
+        webTestClient.put()
+                .uri(CategoryController.BASE_URL + SOME_ID)
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus().isOk();
     }
 }
